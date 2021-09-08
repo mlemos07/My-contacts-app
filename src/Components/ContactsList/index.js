7525d33d32f20327b8be12f488406fd4c5ecda0b
Marianna Lemos <marianalemos07@hotmail.com>
2021-09-08 16:11:52 -0300

    development from header from contactsList

---
+++ src/Components/ContactsList/index.js
@@ -0,0 +1,12 @@
+import { Container, Header } from './styles';
+
+const ContactsList = () => (
+  <Container>
+    <Header>
+      <strong>3 contatos</strong>
+      <a href="/">Novo contato</a>
+    </Header>
+  </Container>
+);
+
+export default ContactsList;
