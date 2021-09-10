dde00a046ebceea16cd630392b8da974a5cadba5
Marianna Lemos <marianalemos07@hotmail.com>
2021-09-10 15:20:43 -0300

    card development for list contacts

--- src/Components/ContactsList/styles.js
+++ src/Components/ContactsList/styles.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
   justify-content: space-between;
 
   strong {
-    color: #222;
     font-size: 24px;
   }
 
@@ -26,6 +25,76 @@ export const Header = styled.header`
     &:hover {
       background: ${({ theme }) => theme.colors.primary.main};
       color: #FFF;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-right: 8px;
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, .4);
+  padding: 16px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 16px
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+      margin-bottom: 4px;
+
+      small {
+        color: ${({ theme }) => theme.colors.primary.main};
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        padding: 4px;
+        border-radius: 4px;
+        font-weight: bold;
+        margin-left: 8px;
+        text-transform: uppercase;
+      }
+    }
+    span {
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
+
     }
   }
 `;
