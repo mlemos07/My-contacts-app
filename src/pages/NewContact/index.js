2678174d13f477b7e8f685d4d71f71ca1f332061
Marianna Lemos <marianalemos07@hotmail.com>
2021-09-12 11:28:54 -0300

    development of text input and select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,15 @@
+import Input from '../../Components/Input';
 import PageHeader from '../../Components/PageHeader';
+import Select from '../../Components/Select';
 
 const NewContact = () => (
-  <PageHeader title="Novo contato" />
+  <>
+    <PageHeader title="Novo contato" />
+    <Input type="text" placeholder="Nome" />
+    <Select>
+      <option value="test1">test1</option>
+    </Select>
+  </>
 );
 
 export default NewContact;
