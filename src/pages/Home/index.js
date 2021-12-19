e8f0ffb580bf4c8f76dd3a8fbffc39702c394e6e
Marianna Lemos <marianalemos07@hotmail.com>
2021-12-19 20:05:59 -0300

    add function to get contacts from server

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,47 +1,66 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Card, Container, Header, InputSearchContainer, ListContainer,
 } from './styles';
 import Arrow from '../../assets/images/Icons/Arrow.svg';
-import EditIcon from '../../assets/images/Icons/EditIcon.svg';
 import TrashIcon from '../../assets/images/Icons/DeleteIcon.svg';
+import EditIcon from '../../assets/images/Icons/EditIcon.svg';
 
-const Home = () => (
-  <Container>
-    <InputSearchContainer>
-      <input type="text" placeholder="Pesquisar contato..." />
-    </InputSearchContainer>
-    <Header>
-      <strong>3 contatos</strong>
-      <Link to="/new">Novo contato</Link>
-    </Header>
-    <ListContainer>
-      <header>
-        <button type="button">
-          <span>Nome</span>
-          <img src={Arrow} alt="Ordenar lista pelo nome" />
-        </button>
-      </header>
-      <Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong>Marianna Lemos</strong>
-            <small>Instagram</small>
-          </div>
-          <span>Marianalemos07@hotmail.com</span>
-          <span>(41) 99999-9999</span>
-        </div>
-        <div className="actions">
-          <Link to="/edit/vbdgfv">
-            <img src={EditIcon} alt="Editar contato" />
-          </Link>
+const Home = () => {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    const getContacts = async () => {
+      const response = await fetch('http://localhost:3000/contacts');
+      const json = await response.json();
+      setContacts(json);
+    };
+    getContacts();
+  }, []);
+  const amoutContacts = contacts.length;
+  const amountEqualToOne = amoutContacts === 1;
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+      <Header>
+        <strong>
+          {amoutContacts}
+          {amountEqualToOne ? ' contato' : ' contatos'}
+        </strong>
+        <Link to="/new">Novo contato</Link>
+      </Header>
+      <ListContainer>
+        <header>
           <button type="button">
-            <img src={TrashIcon} alt="Deletar contato" />
+            <span>Nome</span>
+            <img src={Arrow} alt="Ordenar lista pelo nome" />
           </button>
-        </div>
-      </Card>
-    </ListContainer>
-  </Container>
-);
+        </header>
+        {contacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="info">
+              <div className="contact-name">
+                <strong>{contact.name}</strong>
+                <small>{contact.category.name}</small>
+              </div>
+              <span>{contact.email}</span>
+              <span>{contact.phone}</span>
+            </div>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={EditIcon} alt="Editar contato" />
+              </Link>
+              <button type="button">
+                <img src={TrashIcon} alt="Deletar contato" />
+              </button>
+            </div>
+          </Card>
+        ))}
+      </ListContainer>
+    </Container>
+  );
+};
 
 export default Home;
