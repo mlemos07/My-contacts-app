235a577137f1b5751d2c306376a3559091870051
Marianna Lemos <marianalemos07@hotmail.com>
2022-01-09 14:34:14 -0300

    add service to order

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,53 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Card, Container, Header, InputSearchContainer, ListContainer,
-} from './styles';
 import Arrow from '../../assets/images/Icons/Arrow.svg';
 import TrashIcon from '../../assets/images/Icons/DeleteIcon.svg';
 import EditIcon from '../../assets/images/Icons/EditIcon.svg';
+import Loader from '../../Components/Loader';
+import ContactsService from '../../services/ContactsService';
+import { compareTerms } from '../../utils/string';
+import {
+  Card, Container, Header, InputSearchContainer, ListHeader,
+} from './styles';
 
 const Home = () => {
   const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const changeSearchTermHandler = (event) => setSearchTerm(event.target.value);
   useEffect(() => {
     const getContacts = async () => {
-      const response = await fetch('http://localhost:3000/contacts');
-      const json = await response.json();
-      setContacts(json);
+      setIsLoading(true);
+      try {
+        const contactsList = await ContactsService.listContacts(orderBy);
+        setContacts(contactsList);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
     };
     getContacts();
-  }, []);
-  const amoutContacts = contacts.length;
+  }, [orderBy]);
+  const toggleOrderByHandler = () => {
+    const orderByAsc = orderBy === 'asc';
+    if (orderByAsc) {
+      setOrderBy('desc');
+      return;
+    }
+    setOrderBy('asc');
+  };
+  const filteredContacts = useMemo(() => (
+    contacts.filter((contact) => compareTerms(contact.name, searchTerm))),
+  [contacts, searchTerm]);
+  const amoutContacts = filteredContacts.length;
   const amountEqualToOne = amoutContacts === 1;
   return (
     <Container>
+      <Loader isLoading={isLoading} />
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato..." />
+        <input type="text" value={searchTerm} placeholder="Pesquisar contato..." onChange={changeSearchTermHandler} />
       </InputSearchContainer>
       <Header>
         <strong>
@@ -31,34 +56,36 @@ const Home = () => {
         </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
-      <ListContainer>
-        <header>
-          <button type="button">
-            <span>Nome</span>
-            <img src={Arrow} alt="Ordenar lista pelo nome" />
-          </button>
-        </header>
-        {contacts.map((contact) => (
-          <Card key={contact.id}>
-            <div className="info">
-              <div className="contact-name">
-                <strong>{contact.name}</strong>
-                <small>{contact.category.name}</small>
-              </div>
-              <span>{contact.email}</span>
-              <span>{contact.phone}</span>
+      {!!amoutContacts && (
+        <ListHeader orderBy={orderBy}>
+          <header>
+            <button type="button" onClick={toggleOrderByHandler}>
+              <span>Nome</span>
+              <img src={Arrow} alt="Ordenar lista pelo nome" />
+            </button>
+          </header>
+        </ListHeader>
+      )}
+      {filteredContacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              <small>{contact.category.name}</small>
             </div>
-            <div className="actions">
-              <Link to={`/edit/${contact.id}`}>
-                <img src={EditIcon} alt="Editar contato" />
-              </Link>
-              <button type="button">
-                <img src={TrashIcon} alt="Deletar contato" />
-              </button>
-            </div>
-          </Card>
-        ))}
-      </ListContainer>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
+          </div>
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src={EditIcon} alt="Editar contato" />
+            </Link>
+            <button type="button">
+              <img src={TrashIcon} alt="Deletar contato" />
+            </button>
+          </div>
+        </Card>
+      ))}
     </Container>
   );
 };
