362100e5ca70ecf0fcfe26d4b69931198d011a5f
Marianna Lemos <marianalemos07@hotmail.com>
2022-02-23 21:03:10 -0300

    add class to make requests to the api

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,9 +1,12 @@
+import HttpClient from './utils/HttpClient';
+
 class ContactsService {
-  async listContacts(orderBy = 'asc') {
-    const response = await fetch(
-      `http://localhost:3000/contacts?orderBy=${orderBy}`,
-    );
-    return response.json();
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3000');
+  }
+
+  listContacts(orderBy = 'asc') {
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 }
 
---
+++ src/services/utils/HttpClient.js
@@ -0,0 +1,22 @@
+import APIError from '../../Errors/APIError';
+
+class HttpClient {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  async get(path) {
+    const response = await fetch(`${this.baseURL}${path}`);
+    const contentType = response.headers.get('Content-type');
+    let body = null;
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
+    if (response.ok) {
+      return body;
+    }
+    throw new APIError(response, body);
+  }
+}
+
+export default HttpClient;
