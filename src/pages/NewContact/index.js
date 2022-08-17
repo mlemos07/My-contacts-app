ea94b10ff6dbc21b38ca540a410de4cb70689e41
Marianna Lemos <marianalemos07@hotmail.com>
2022-08-17 16:15:53 -0300

    add function to create contact

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,11 +1,27 @@
 import ContactForm from '../../Components/ContactForm';
 import PageHeader from '../../Components/PageHeader';
+import ContactsService from '../../services/ContactsService';
 
-const NewContact = () => (
-  <>
-    <PageHeader title="Novo contato" />
-    <ContactForm buttonLabel="Cadastrar" />
-  </>
-);
+const NewContact = () => {
+  const handleSubmit = async ({
+    name, email, phone, categoryId,
+  }) => {
+    try {
+      const contact = {
+        name,
+        email,
+        phone,
+        category_id: categoryId,
+      };
+      await ContactsService.createContact(contact);
+    } catch {}
+  };
+  return (
+    <>
+      <PageHeader title="Novo contato" />
+      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
+    </>
+  );
+};
 
 export default NewContact;
