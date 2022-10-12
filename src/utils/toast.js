4c29005b8f3d7e16f5df82f38e8ec1ce94fc8bbb
Marianna Lemos <marianalemos07@hotmail.com>
2022-10-11 21:25:10 -0300

    add toastEventMenager instance

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,11 +1,7 @@
-const toast = ({ type, text }) => {
-  const event = new CustomEvent('addtoast', {
-    detail: {
-      type,
-      text,
-    },
-  });
-  document.dispatchEvent(event);
-};
+import EventManager from '../lib/EventManager';
+
+export const toastEventMenager = new EventManager();
+
+const toast = ({ type, text }) => toastEventMenager.emit('addtoast', { type, text });
 
 export default toast;
