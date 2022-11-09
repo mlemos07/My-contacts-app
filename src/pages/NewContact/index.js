39ea36278b3fec698c473cdb84fa9bc1f4497fa4
Marianna Lemos <marianalemos07@hotmail.com>
2022-11-09 20:14:41 -0300

    reset form after edit contact

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,11 @@
+import { useRef } from 'react';
 import ContactForm from '../../Components/ContactForm';
 import PageHeader from '../../Components/PageHeader';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 const NewContact = () => {
+  const contactFormRef = useRef(null);
   const handleSubmit = async ({
     name, email, phone, categoryId,
   }) => {
@@ -15,6 +17,7 @@ const NewContact = () => {
         category_id: categoryId,
       };
       await ContactsService.createContact(contact);
+      contactFormRef.current.resetField();
       toast({ type: 'success', text: 'Contato criado com sucesso.' });
     } catch {
       toast({
@@ -26,7 +29,11 @@ const NewContact = () => {
   return (
     <>
       <PageHeader title="Novo contato" />
-      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Cadastrar"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
