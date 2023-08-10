913d56d7f820343a4f2d3389bead7f4443aa0d7b
Marianna Lemos <marianalemos07@hotmail.com>
2023-08-10 08:43:00 -0300

    refactor element to component pattern

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,52 +1,12 @@
-import { useHistory, useParams } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
 import ContactForm from '../../Components/ContactForm';
 import Loader from '../../Components/Loader';
 import PageHeader from '../../Components/PageHeader';
-import ContactsService from '../../services/ContactsService';
-import toast from '../../utils/toast';
-import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import useEditContact from './useEditContact';
 
 const EditContact = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [contactName, setContactName] = useState('');
-  const contactFormRef = useRef(null);
-  const { id } = useParams();
-  const history = useHistory();
-  const safeAsyncAction = useSafeAsyncAction();
-  useEffect(() => {
-    const loadContact = async () => {
-      try {
-        const contactData = await ContactsService.getContactById(id);
-        safeAsyncAction(() => {
-          contactFormRef.current.setFieldValues(contactData);
-          setContactName(contactData.name);
-          setIsLoading(false);
-        });
-      } catch {
-        safeAsyncAction(() => {
-          history.push('/');
-          toast({
-            type: 'danger',
-            text: 'Contato não encontrado.',
-          });
-        });
-      }
-    };
-    loadContact();
-  }, [id, history, safeAsyncAction]);
-  const handleSubmit = async (contact) => {
-    try {
-      await ContactsService.updateContact(id, contact);
-      setContactName(contact.name);
-      toast({ type: 'success', text: 'Contato editado com sucesso.' });
-    } catch {
-      toast({
-        type: 'danger',
-        text: 'Ocorreu um erro ao editar o contato.',
-      });
-    }
-  };
+  const {
+    isLoading, contactName, contactFormRef, handleSubmit,
+  } = useEditContact();
   return (
     <>
       <Loader isLoading={isLoading} />
